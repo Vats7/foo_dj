bf590f644a78b59ea24de1df43c1c78f6f6e2fb0
vats7 <vatsalyasharma92@gmail.com>
2020-09-07 03:32:29 -0700

    updated db settings

--- foo/foo/settings.py
+++ foo/foo/settings.py
@@ -79,17 +79,28 @@ WSGI_APPLICATION = 'foo.wsgi.application'
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
-
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'foodb',
-            'USER': 'vatsalyasharma',
-            'PASSWORD': 'footballerize',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         }
     }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'foodb',
+                'USER': 'vatsalyasharma',
+                'PASSWORD': 'footballerize',
+                'HOST': 'localhost',
+                'PORT': '5432',
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
